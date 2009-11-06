c472f1957222fcde610e5c5be04ae4f8a2a51b39
anton.danilchenko <anton.danilchenko@b75869fe-b9ac-11de-bda7-396a5af1c03d>
2009-11-06 14:55:54 +0000

    Fix bug in Templum: throw exception if template contein php tags.

--- trunk/generator/cli.php
+++ trunk/generator/cli.php
@@ -1,4 +1,349 @@
+#!/usr/bin/php
 <?php
 /**
  * Generate application in CLI (command line).
+ *
+ * FIRST USE
+ *  - change path to php executable in first line (unix: /usr/bin/php, windows: C:\php\php.exe)
+ *  - open command line tool (unix: gnome-terminal, windows: C:\WINDOWS\system32\cmd.exe). Next - work in terminal
+ *  - go to you project root dir (~/workbench/project-name)
+ *  - start generator - type path to this file (~/workbench/probus/Probus/Tools/Generator/Generator.php)
+ *  - type available command
+ *
+ * INSTRUCTION
+ *  - start command line tool (unix: terminal, windows: cmd.exe)
+ *  - go to site application directory in console
+ *  - type path to project generator (/path/to/Generate.php)
+ *  - press Enter
+ *  - type available command
+ *
+ * COMMANDS
+ *  - "help": show help
+ *  - "project": work with project
+ *    - "init": create empty project structure (set correct chmod for log and cache dirs)
+ *    - "check": show warning messages for project files
+ *    - "repair": check project and add not exist files
+ *    - "clear": clear project (backup all files)
+ *      - "cache": delete all cached templates
+ *  - "page": work with pages in service
+ *    - "add": create pages (config, content template, frame template, content controller)
+ *    - "del": delete pages (move it to backup)
+ *    - "rename": rename pages (backup file with old name)
+ *    - "clear": clear pages (backup all files)
+ *  - "block": work with blocks
+ *    - "add": create blocks (config, block template, block controller)
+ *    - "del": delete blocks (move it to backup)
+ *    - "rename": rename blocks (backup file with old name)
+ *    - "clear": clear blocks (backup all files)
+ *  - "exit": exit
+ *
+ * @author Anton Danilchenko <anton.danilchenko@gmail.com>
+ * @license &copy; Anton Danilchenko
+ * @link http://probus.googlecode.com
+ * @package Tools
+ * @category Probus
+ * @version 0.9
  */
+class Phpdays_Tool_Generator {
+	/**
+	 * Link to itself object.
+	 *
+	 * @var Probus_Tools_Generator
+	 */
+	private static $_oInstatce = null;
+	/**
+	 * Project directory.
+	 *
+	 * @var string
+	 */
+	private $_sProjectDir = '';
+
+	public $sMessage = '';
+
+
+	/**
+	 * Create current class object (create once).
+	 *
+	 * @return Probus
+	 */
+	public static function singleton() {
+		// create itself only once
+		if (is_null(self::$_oInstatce)) {
+			$sClassName = __CLASS__;
+			self::$_oInstatce = new $sClassName();
+		}
+		// return link to itself object
+		return self::$_oInstatce;
+	}
+
+
+	/**
+	 * Create new controller with actions.
+	 *
+	 * Always create action "index".
+	 *
+	 * @param $sController string: Controller name
+	 * @param $aActions array: Action names
+	 * @param $bReplace bool: Replace action file if exists
+	 * @return
+	 */
+	public function add($sController, array $aActions=array(), $bReplace=false) {
+
+		list($sType,$sControllerName)=explode('/',$sController);
+
+		switch ($sType) {
+			case 'site':
+				$sApplicationPath=$this->_sProjectDir."";
+				if (! is_dir($sApplicationPath))
+					if (! mkdir($sApplicationPath,0755))
+						throw new Exception("Directory '{$sApplicationPath}' not created");
+				$sPublicPath=$this->_sProjectDir."/public";
+				if (! is_dir($sPublicPath))
+					if (! mkdir($sPublicPath))
+						throw new Exception("Directory '{$sPublicPath}' not created");
+
+				$sSitePath=dirname(__FILE__).'/site';
+				$this->_copyDir($sSitePath,$sApplicationPath);
+
+				$sIndexFileData=file_get_contents($sPublicPath.'/index.php');
+				$fp=fopen($sPublicPath.'/index.php','w');
+				if($fp) {
+					$sIndexFileData=str_replace('%CoreDir%',realpath(dirname(__FILE__).'/../../../').'/',$sIndexFileData);
+					fwrite($fp,$sIndexFileData);
+					fclose($fp);
+				}
+				return true;
+			break;
+
+			case 'content':
+			case 'block':
+			case 'form':
+
+				$sTControllerPath=dirname(__FILE__).'/site/application/'.$sType.'/'.$sType;
+				$sTAction=$sType;
+				if( ($sType=='content') ) {
+
+					$sTControllerPath=dirname(__FILE__).'/site/application/'.$sType.'/index';
+					$sTAction='index';
+
+					// set default path
+					if( (empty($aActions) || ! in_array('index', $aActions) ) ) {
+						$aActions[] = 'index';
+					}
+				}
+
+				// set correct path
+				$sControllerPath = "{$this->_sProjectDir}/application/{$sController}";
+				// create service directory
+				if (! is_dir($sControllerPath))
+					if (! mkdir($sControllerPath))
+						throw new Exception("Directory '{$sControllerPath}' not created");
+
+				$this->_copyDir($sTControllerPath,$sControllerPath);
+
+				// create all action files
+				foreach ($aActions as $sAction) {
+					$this->addAction($sTControllerPath,$sTAction,$sControllerPath, $sAction, $bReplace);
+				}
+
+				$this->add('frame/'.$sControllerName, array(), $bReplace);
+
+				return true;
+			break;
+
+			case 'frame':
+
+				$sTFramePath=dirname(__FILE__).'/site/application/'.$sType;
+				$sTAction='index';
+
+				$sFramePath = "{$this->_sProjectDir}/application/{$sType}";
+				$this->addAction($sTFramePath,$sTAction,$sFramePath, $sControllerName, $bReplace);
+
+				echo "Frame {$sControllerName} is added\n";
+				return true;
+			break;
+
+			default:
+				;
+			break;
+		}
+	}
+
+
+	/**
+	 * Create new action.
+	 *
+	 * @param $sTControllerPath
+	 * @param $sTAction
+	 * @param $sControllerPath
+	 * @param $sAction
+	 * @param $bReplace
+	 * @return bool
+	 */
+	public function addAction($sTControllerPath, $sTAction, $sControllerPath, $sAction='index', $bReplace=false) {
+		// set default action
+		if(empty($sAction))
+			$sAction='index';
+		// set path
+		$sTActionPath = "{$sTControllerPath}/{$sTAction}.php";
+		$sActionPath = "{$sControllerPath}/{$sAction}.php";
+		if (file_exists($sActionPath)) {
+			// back up file
+		}
+		else {
+			// create action file
+			if(@copy($sTActionPath,$sActionPath)) {
+				echo "Action {$sActionPath} is added\n";
+			} else {
+				echo "Fail! Action {$sActionPath} is not added\n";
+			}
+		}
+		$sTActionTemplPath = "{$sTControllerPath}/{$sTAction}.html";
+		$sActionTemplPath = "{$sControllerPath}/{$sAction}.html";
+		if (file_exists($sActionTemplPath)) {
+			// back up file
+		}
+		else {
+			// create template file
+			if (@copy($sTActionTemplPath,$sActionTemplPath)) {
+				echo "Action template {$sActionTemplPath} is added\n";
+			} else {
+				echo "Fail! Action template {$sActionTemplPath} is not added\n";
+			}
+		}
+		$sTActionTemplConfigPath = "{$sTControllerPath}/{$sTAction}.html.conf";
+		$sActionTemplConfigPath = "{$sControllerPath}/{$sAction}.html.conf";
+		// check file on disk
+		if (file_exists($sActionTemplConfigPath)) {
+			// back up file
+		}
+		else {
+			// craete config file for action
+			if(@copy($sTActionTemplConfigPath, $sActionTemplConfigPath)) {
+				echo "Actions config {$sActionTemplConfigPath} is added\n";
+			} else {
+				echo "Fail! Actions config {$sActionTemplConfigPath} is not added\n";
+			}
+		}
+	}
+
+
+	protected function _copyDir($sDirOut,$sDirIn) {
+
+		if(! is_dir($sDirIn)){
+			if (!@mkdir($sDirIn,0755)) {
+				return false;
+			}
+		}
+
+		if(! is_dir($sDirOut) || ! is_dir($sDirIn)) {
+			return false;
+		}
+
+		if ($dir=opendir($sDirOut)){
+			while ($filename=readdir($dir)){
+
+				if (($filename==".")||($filename=="..")){
+					continue;
+				}
+
+				if (is_dir($sDirOut.'/'.$filename)){
+					$this->_copyDir($sDirOut.'/'.$filename,$sDirIn.'/'.$filename);
+				} else {
+					copy($sDirOut.'/'.$filename,$sDirIn.'/'.$filename);
+				}
+			}
+
+			closedir($dir);
+		}
+	}
+
+
+	/**
+	 * Set path to current project directory.
+	 *
+	 * @return Probus_Tools_Generator
+	 */
+	private function __construct() {
+		// check project directory
+		$this->_sProjectDir = rtrim(getcwd(), '/');
+		if (! is_writable($this->_sProjectDir))
+			throw new Exception("Current directory '{$this->_sProjectDir}' not writable");
+	}
+
+
+//	private function _create
+}
+
+// start application
+try {
+	// create generator object
+	$oGenerator = Phpdays_Tool_Generator::singleton();
+	// start screen (introduction)
+	print "\tCREATE PROJECT MODULES\n";
+	print "\n";
+	print "Syntax: COMMAND controllerName: action1 action2 action3\n";
+	print "Example: ADD user: register login logout new\n";
+	print "\n";
+	print "Commands:\n";
+	print "  - ADD: create new controller with actions (always create default action 'index')\n";
+	print "  - DEL: delete actions in controller. Backup files save in directory 'backup'\n";
+	print "  - REPLACE: replace selected actions in controller to new empty actions. Backup files save in directory 'backup'\n";
+	print "  - CHECK: check files structure (all files in project on relations) and show log\n";
+	print "\n";
+	print "Press Enter for exit (no input module name)\n";
+	// handle user commands
+	do {
+		// read command
+		print 'Enter command: ';
+		// reads one line from STDIN
+		$sCommandLine = trim(fgets(STDIN));
+		// execute command
+		if ('' != $sCommandLine) {
+			// clear string
+			$sCommandLine = str_replace(array(',',';',':','\\'), '', $sCommandLine);
+			// parse string
+			$aCommandLineParams = explode(' ', $sCommandLine);
+			foreach ($aCommandLineParams as &$sParam)
+				$sParam = strtolower(trim($sParam));
+			// set correct params
+			$sCommand = array_shift($aCommandLineParams);
+			$sController = array_shift($aCommandLineParams);
+			// execute command
+			switch ($sCommand) {
+				case 'add':
+				case 'create':
+					// create actions
+					if($oGenerator->add($sController, $aCommandLineParams)) {
+						echo $sController." is added.\n";
+					} else {
+						echo "Fail!".$sController." is not added.\n";
+					};
+					break;
+				case 'del':
+				case 'delete':
+				case 'rem':
+				case 'remove':
+					// backup actions
+					// remove actions
+					$oGenerator->del($sController, $aCommandLineParams);
+					// remove controller dir (if empty)
+					break;
+				case 'replace':
+					// backup data
+					// create empty actions
+					$oGenerator->add($sController, $aCommandLineParams, true);
+					break;
+				case 'check':
+					break;
+				default:
+					print "Command not supported!\n";
+			}
+		}
+	} while('' != $sCommandLine);
+}
+catch (Exception $oEx) {
+	print $oEx->getMessage();
+	print "\n";
+}
+print "Generator end work\n";
