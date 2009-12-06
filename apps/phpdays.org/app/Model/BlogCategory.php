33a5bf4f0f11606ce4949c79e343021b7130033d
anton.danilchenko <anton.danilchenko@b75869fe-b9ac-11de-bda7-396a5af1c03d>
2009-12-06 22:31:17 +0000

    [apps/phpdays.org] Fix model extend.

--- apps/phpdays.org/app/Model/BlogCategory.php
+++ apps/phpdays.org/app/Model/BlogCategory.php
@@ -4,7 +4,7 @@
  *
  * @author Anton Danilchenko
  */
-class Model_BlogCategory {
+class Model_BlogCategory extends Days_Model {
     protected $_name     = 'type: String, min: 2, max: 50, required';
     protected $_url      = 'type: String, min: 2, max: 50, required';
 }
--- apps/phpdays.org/app/Model/BlogTag.php
+++ apps/phpdays.org/app/Model/BlogTag.php
@@ -4,7 +4,7 @@
  *
  * @author Anton Danilchenko
  */
-class Model_BlogTag {
+class Model_BlogTag extends Days_Model {
     protected $_name = 'type: String, min: 2, max: 70, required, unique';
     protected $_url  = 'type: String, min: 2, max: 70, required, unique';
 }
