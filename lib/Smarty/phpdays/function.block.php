506552a575fd587d69486984be5d5d9503318b67
xaoc2007 <xaoc2007@b75869fe-b9ac-11de-bda7-396a5af1c03d>
2009-12-09 16:19:42 +0000

    finish extension smarty 'block', added to a controller unit for generating data for template block example for the block {block name = 'lab / menu'} you can create a class Controller_Block_Lab_Menu in the init () method to determine the necessary variables for the template/View/block/lab/menu.html

--- lib/Smarty/phpdays/function.block.php
+++ lib/Smarty/phpdays/function.block.php
@@ -4,12 +4,12 @@
  *
  * Part of "php:Days - php5 framework" project (http://phpdays.googlecode.com).
  *
- * @copyright	Copyright (c) 2009 phpDays foundation (http://phpdays.org)
- * @license	http://www.opensource.org/licenses/mit-license.php The MIT License
- * @link	http://phpdays.googlecode.com/
- * @package	phpDays
- * @subpackage	phpDays Smarty library
- * @author	Anton Danilchenko <happy@phpdays.org>
+ * @copyright    Copyright (c) 2009 phpDays foundation (http://phpdays.org)
+ * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link    http://phpdays.googlecode.com/
+ * @package    phpDays
+ * @subpackage    phpDays Smarty library
+ * @author    Anton Danilchenko <happy@phpdays.org>
  * @param       Parameters
  *  - name: name of block (placed in `View/block` directory)
  *  - to: name of variable to assign result
@@ -18,13 +18,33 @@ function smarty_function_block($params, Smarty &$smarty) {
     // check required parameters
     if (! isset ($params['name']))
         throw new Days_Exception('Not passed `name` parameter in smarty plugin `block`');
-    // set correct name
-    $path = "block/{$params['name']}.html";
+    // set template path
+    $template = "block/{$params['name']}.html";
     // check file
-    if (! $smarty->template_exists($path))
-        throw new Days_Exception("Not found file `{$path}`");
-    // get content
-    $content = $smarty->fetch($path);
+    if (! $smarty->template_exists($template))
+        throw new Days_Exception("Not found file `{$template}`");
+
+    //specify the name of the controller
+    $className = $params['name'];
+    $classPathParts = explode('/', $className);
+    for ($i=0; $i<count($classPathParts); $i++) {
+        $classPathParts[$i] = ucfirst($classPathParts[$i]);
+    }
+    $controller = implode('_', $classPathParts);
+    $controllerClass = 'Controller_Block_' . $controller;
+    //execute of the controller, if it exists
+    if(class_exists($controllerClass)) {
+        // create controller
+        $controllerObj = new $controllerClass($template);
+        if (! $controllerObj instanceof Days_Controller)
+            throw new Days_Exception("Controller '{$controllerClass}' should be extended from 'Days_Controller'");
+        // call init() method for prepare object
+        $controllerObj->init();
+        $content = $controllerObj->getContent(false);
+        unset($controllerObj);
+    } else {
+        $content = $smarty->fetch($template);
+    }
     // set to variable
     if (isset ($params['to']))
         $smarty->assign($params['to'], $content);
