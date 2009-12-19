50b59ab086085ba52eea579462c9fb82d498708d
anton.danilchenko <anton.danilchenko@b75869fe-b9ac-11de-bda7-396a5af1c03d>
2009-12-19 13:17:46 +0000

    Added call of Days_Controller methods for show how its works

--- trunk/apps/phpdays.org/app/Controller/Index.php
+++ trunk/apps/phpdays.org/app/Controller/Index.php
@@ -22,6 +22,19 @@ class Controller_Index extends Days_Controller {
         // find blog posts
         $blogPosts = new Model_BlogPost();
         $blogPosts->limit(10)->sort('date DESC');
+        // work with environment
+        // get values
+        $userName   = $this->post('name') OR 'noname';
+        $sessEmail  = $this->session('email') OR '';
+        $urlAddress = $this->url('address') OR '';
+        // set value
+        $this->post('name', 'Anton');
+        $this->session('email', 'test@mail.com');
+        $this->url('address', 'Ukraine, Dnipropetrovsk');
+        // send message to developer (in firebug or in log file)
+        $this->log('All right. All work well!');
+        // add event handler (need create method for handle this event)
+        $this->event('user/logged', array($this, '_onUserLogin'));
         // set data to template
         $this->posts = $blogPosts;
     }
