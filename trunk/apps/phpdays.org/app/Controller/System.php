b29883bfcaf0eee3f5cea14bc3ab52f5ac7f3900
anton.danilchenko <anton.danilchenko@b75869fe-b9ac-11de-bda7-396a5af1c03d>
2009-12-24 16:51:10 +0000

    Chenged System controller in application phpdays.org

---
+++ trunk/apps/phpdays.org/app/Controller/System.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * System class - determines the instructions that must be done immediately
+ * after the kernel initialized.
+ *
+ * Use "php:Days - php5 framework" (http://phpdays.googlecode.com).
+ *
+ * @copyright   Copyright (c) 2009 phpDays foundation (http://phpdays.org)
+ * @license     http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link        http://phpdays.googlecode.com/
+ * @package     phpDays
+ * @subpackage  application
+ * @author      Alexandr Janzen <xaoc2007@gmail.com>
+ */
+class Controller_System {
+
+    /**
+     * Defines a list of subscribers to the event
+     */
+    public static function autorun() {}
+}
